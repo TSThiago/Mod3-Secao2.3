5834a9046a9efb2c3cbaea13032761dac27a2a53
Luiz Pachioni <lopachioni@gmail.com>
2022-12-05 21:24:43 -0300

    testes para calculo de juros compostos

--- luiz/index.test.js
+++ luiz/index.test.js
@@ -1,17 +1,33 @@
 const index = require("../thiago/index")
 
 test('calculo de juros simples, 2% sobre 1000 durante 1 ano', () =>{
-    expect(calcularJurosSimples(1000, 2, 1).toBe(240))
+    expect(index.calcularJurosSimples(1000, 2, 1).toBe(240))
 });
 
 test('calculo de juros simples, erro porque valor inicial não é inteiro', () =>{
-    expect(calcularJurosSimples(1000.50, 2, 1).toBeFalsy())
+    expect(index.calcularJurosSimples(1000.50, 2, 1).toBeFalsy())
 });
 
 test('calculo de juros simples, erro porque valor inicial não é positivo', () =>{
-    expect(calcularJurosSimples(-1000, 2, 1).toBeFalsy())
+    expect(index.calcularJurosSimples(-1000, 2, 1).toBeFalsy())
 });
 
 test('calculo de juros simples, erro porque valor inicial não é positivo nem um inteiro', () =>{
-    expect(calcularJurosSimples(-1000.50, 2, 1).toBeFalsy())
+    expect(index.calcularJurosSimples(-1000.50, 2, 1).toBeFalsy())
+});
+
+test('calculo de juros compostos, 2% sobre 1000 durante 1 ano', () =>{
+    expect(index.calcularJurosCompostos(1000, 2, 1).toBe(1268.24))
+});
+
+test('calculo de juros compostos, erro porque valor inicial não é inteiro', () =>{
+    expect(index.calcularJurosCompostos(1000.50, 2, 1).toBeFalsy())
+});
+
+test('calculo de juros compostos, erro porque valor inicial não é positivo', () =>{
+    expect(index.calcularJurosCompostos(-1000, 2, 1).toBeFalsy())
+});
+
+test('calculo de juros compostos, erro porque valor inicial não é positivo nem um inteiro', () =>{
+    expect(index.calcularJurosCompostos(-1000.50, 2, 1).toBeFalsy())
 });
